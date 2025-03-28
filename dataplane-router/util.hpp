49d17163ef64b8bba56b0321baf13668a7d204a6
Albert24GG <guiman.albert09@gmail.com>
2025-03-28 21:34:07 +0200

    feat: Add byte swapping utility

---
+++ dataplane-router/util.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <type_traits>
+
+namespace router::util {
+
+template <auto T, auto...> constexpr auto deferred_value = T;
+
+template <typename T, typename = std::enable_if_t<std::is_integral_v<T> &&
+                                                  std::is_unsigned_v<T>>>
+T constexpr hton(T value) {
+
+#if __BYTE_ORDER__ == __BIG_ENDIAN
+  return value;
+#else
+  constexpr auto t_size = sizeof(T);
+  if constexpr (t_size == 1) {
+    return value;
+  } else if constexpr (t_size == 2) {
+    return __builtin_bswap16(value);
+  } else if constexpr (t_size == 4) {
+    return __builtin_bswap32(value);
+  } else if constexpr (t_size == 8) {
+    return __builtin_bswap64(value);
+  } else {
+    // Use a deferred false static_assert to avoid compilation errors
+    static_assert(deferred_value<false>, "Unsupported type for hton");
+  }
+#endif
+}
+
+template <typename T> constexpr T ntoh(T value) { return hton(value); }
+
+} // namespace router::util
