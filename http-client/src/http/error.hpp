f465d76b03b7204f652006ea4351172439f0ca98
Albert24GG <guiman.albert09@gmail.com>
2025-05-11 23:21:34 +0300

    Add `to_str()` function for `http::Error`

--- http-client/src/http/error.hpp
+++ http-client/src/http/error.hpp
@@ -14,4 +14,29 @@ enum class Error {
   WriteTimeout
 };
 
+constexpr auto to_str(Error error) {
+  switch (error) {
+  case Error::Success:
+    return "Success (no error)";
+  case Error::Connection:
+    return "Could not establish connection";
+  case Error::ConnectionTimeout:
+    return "Connection timed out";
+  case Error::HostNotFound:
+    return "Host not found";
+  case Error::Read:
+    return "Failed to read from socket";
+  case Error::ReadTimeout:
+    return "Socket read timed out";
+  case Error::Write:
+    return "Failed to write to socket";
+  case Error::WriteTimeout:
+    return "Socket write timed out";
+  case Error::Unknown:
+    return "Unknown error";
+  default:
+    return "Invalid error code";
+  }
 }
+
+} // namespace http
